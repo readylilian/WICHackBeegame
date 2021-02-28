fdad6fbdaa7833b87e7a48a977410891a91fbae2
Lily <lr4631@rit.edu>
2021-02-27 23:17:26 -0500

    Update

--- Bee Game/Assets/Scripts/BeeScript.cs
+++ Bee Game/Assets/Scripts/BeeScript.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BeeScript : MonoBehaviour
 {
+    public int honeyFavor = 0;
+    public int bumbleFavor = 0;
+    public int flyFavor = 0;
+    public int carpFavor = 0;
+    public int leafFavor = 0;
+    public int minerFavor = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,30 @@ public class BeeScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddPointHoney()
+    {
+        honeyFavor += 1;
+    }
+    public void AddPointBumble()
+    {
+        bumbleFavor += 1;
+    }
+    public void AddPointFly()
+    {
+        flyFavor += 1;
+    }
+    public void AddPointCarp()
+    {
+        carpFavor += 1;
+    }
+    public void AddPointLeaf()
+    {
+        leafFavor += 1;
+    }
+    public void AddPointMiner()
+    {
+        minerFavor += 1;
     }
 }
